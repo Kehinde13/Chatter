9d6e787a81939cd1b7ed2a9ee3160168ef41b045
Kehinde Balogun <balogunkehinde3@gmail.com>
2024-09-02 02:22:16 -0400

    fixed a style bug

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -10,7 +10,7 @@ const CardContainer = () => {
   return (
     <div className="my-20 w-[80%] mx-auto px-3">
       <h1 className="text-2xl md:text-3xl my-5 md:my-10 mx-2 font-bold">Recent Blog Posts</h1>
-      <div className="flex flex-col md:flex-row gap-10 justify-between flex-wrap">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         {
         recentPost?.map((post: Post, i: number) => <Card post={post} key={i} size="w-[30%]" />)
       } 
